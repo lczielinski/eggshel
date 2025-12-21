9b9d813e462fff91a7846bf3b024f15e6cb5126a
Laura Zielinski <lczielinski1@gmail.com>
2025-12-21 12:12:09 -0500

    fixed runner.py. all in good shape

--- runner.py
+++ runner.py
@@ -4,11 +4,8 @@ import textwrap
 import time
 
 # given a string program (Add (Mul x y) y)
-# generates (let ex (Base 0.0 (Add (Mul (Var "x") (Var "y")) (Var "y"))))
+# generates "(Base 0.0 (Add (Mul (Var "x") (Var "y")) (Var "y")) 0.0)"
 def generate_expr(s):
-    def tokenize(s):
-        return s.replace("(", " ( ").replace(")", " ) ").split()
-    
     def parse(tokens):
         token = tokens.pop(0)
         if token == "(":
@@ -37,23 +34,27 @@ def generate_expr(s):
             return "(" + " ".join(to_string(item) for item in expr) + ")"
         return str(expr)
     
-    tokens = tokenize(s)
+    tokens = s.replace("(", " ( ").replace(")", " ) ").split()
     parsed = parse(tokens)
     replaced = replace_vars(parsed)
-    return "(let ex (Base 0.0 " + to_string(replaced) + " 0.0))"
+    return "(Base 0.0 " + to_string(replaced) + " 0.0)"
 
-# given a string x y z
-# generates (let ctx (Tens (Tens (Base 0.0 (Var "x") x_p) (Base 0.0 (Var "y") y_p)) (Base 0.0 (Var "z") z_p)))
-def generate_ctx(s):
-    parts = s.split()
-    result = f"(Base 0.0 (Var \"{parts[-1]}\") ?{parts[-1]}_p)"
-    
-    for i in range(len(parts) - 2, -1, -1):
-        base = f"(Base 0.0 (Var \"{parts[i]}\") ?{parts[i]}_p)"
-        result = f"(Tens {base} {result})"
-    
+# given ["x", "y"]
+# generates ["(Base 0.0 (Var "x") x_p)", "(Base 0.0 (Var "y") y_p)"]
+def generate_bases(var_names):
+    return [f"(Base 0.0 (Var \"{var}\") {var}_p)" for var in var_names]
+
+# given ["(Base 0.0 (Var "x") x_p)", "(Base 0.0 (Var "y") y_p)"]
+# generates "(Tens (Base 0.0 (Var "x") x_p) (Base 0.0 (Var "y") y_p))"
+def generate_ctx(bases):
+    result = bases[-1]
+    for i in range(len(bases) - 2, -1, -1):
+        result = f"(Tens {bases[i]} {result})"
     return result
 
+# given [x, y, z]
+# generates (rule ((Base 0.0 (Var "x") x_p) (Base 0.0 (Var "y") y_p) (Base 0.0 (Var "z") z_p))
+#   ((Tens (Base 0.0 (Var "x") x_p) (Tens (Base 0.0 (Var "y") y_p) (Base 0.0 (Var "z") z_p))))
 def generate_rule(var_names):
     conditions = " ".join(f"(Base 0.0 (Var \"{var}\") {var}_p)" for var in var_names)
     
@@ -64,23 +65,26 @@ def generate_rule(var_names):
     return f"(rule ({conditions})\n      ({tens_expr}))"
 
 def run(test_name, expr, ctx):
-    var_names = ctx.split()
     expr = generate_expr(expr)
-    ctx = generate_ctx(ctx)
+    var_names = ctx.split()
+    bases = generate_bases(var_names)
+    ctx = generate_ctx(bases)
 
-    rule = generate_rule(var_names)
     bounds_type = " ".join(["String f64"] * len(var_names))
-    bounds_query = " ".join(f"\"{var}\" ?{var}_p" for var in var_names)
+    bounds_query = " ".join(f"\"{var}\" {var}_p" for var in var_names)
 
     file = "tests/" + test_name + ".egg"
     with open(file, "w") as f:
-        f.write(expr + "\n\n")
-        f.write(rule + "\n\n")
+        f.write(f"(let ex {expr})\n\n")
+        f.write(f"(rule ({" ".join(bases)})\n      ({ctx}))\n\n")
+        f.write(f"(rule ((-> ctx2 ex) (-> ctx1 ctx2))\n      ((-> ctx1 ex)))\n\n")
+
         f.write("(run-schedule (saturate (run)))\n\n")
+        f.write("(ruleset post)\n\n")
         
         f.write(f"(relation bounds ({bounds_type}))\n\n")
-        f.write(f"(rule ((-> {ctx} ex))\n      ((bounds {bounds_query})))\n\n")
-        f.write("(run 1)\n\n")
+        f.write(f"(rule ((-> {ctx} ex))\n      ((bounds {bounds_query})) :ruleset post)\n\n")
+        f.write("(run post 1)\n\n")
         f.write("(print-function bounds 10)\n\n")
 
     src_names = ["Definitions", "Context", "Decombiners", "Operations"]
@@ -93,32 +97,24 @@ def run(test_name, expr, ctx):
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         end = time.perf_counter()
         f.write(textwrap.indent(result.stdout, "; "))
-        f.write(f"\n; Took {end - start} seconds")
+        f.write(f"; Took {end - start} seconds")
 
-# if len(sys.argv) != 4:
-#     print(f"Usage: {sys.argv[0]} <test name> <expr> <context>")
-#     sys.exit(1)
-    
-# test_name = sys.argv[1]
-# expr = generate_expr(sys.argv[2])
-# ctx = generate_ctx(sys.argv[3])
-
-# run("linear/linear2", "(Add x (Mul a x))", "x a")
-# run("linear/linear3", "(Add x (Add (Mul a x) (Mul b x)))", "x a b")
+run("linear/linear2", "(Add x (Mul a x))", "x a")
+run("linear/linear3", "(Add x (Add (Mul a x) (Mul b x)))", "x a b")
 run("linear/linear4", "(Add x (Add (Mul a x) (Add (Mul b x) (Mul c x)))))", "x a b c")
 # run("linear/linear5", "(Add x (Add (Mul a x) (Add (Mul b x) (Add (Mul c x) (Mul d x))))))", "x a b c d")
 # run("linear/linear6", "(Add x (Add (Mul a x) (Add (Mul b x) (Add (Mul c x) (Add (Mul d x) (Mul e x)))))))", "x a b c d e")
 
-# run("norm/norm1", "(Sqrt (Mul x x))", "x")
-# run("norm/norm2", "(Sqrt (Add (Mul x x) (Mul y y)))", "x y")
-# run("norm/norm3", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Mul z z))))", "x y z")
-# run("norm/norm4", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Mul a a)))))", "x y z a")
+run("norm/norm1", "(Sqrt (Mul x x))", "x")
+run("norm/norm2", "(Sqrt (Add (Mul x x) (Mul y y)))", "x y")
+run("norm/norm3", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Mul z z))))", "x y z")
+run("norm/norm4", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Mul a a)))))", "x y z a")
 # run("norm/norm5", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Add (Mul a a) (Mul b b))))))", "x y z a b")
 # run("norm/norm6", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Add (Mul a a) (Add (Mul b b) (Mul c c)))))))", "x y z a b c")
 # run("norm/norm7", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Add (Mul a a) (Add (Mul b b) (Add (Mul c c) (Mul d d))))))))", "x y z a b c d")
 
-# run("quad/quad2", "(Add x (Mul x (Mul x a)))", "x a")
-# run("quad/quad3", "(Add x (Add (Mul x (Mul x a)) (Mul x (Mul x b))))", "x a b")
+run("quad/quad2", "(Add x (Mul x (Mul x a)))", "x a")
+run("quad/quad3", "(Add x (Add (Mul x (Mul x a)) (Mul x (Mul x b))))", "x a b")
 # run("quad/quad4", "(Add x (Add (Mul x (Mul x a)) (Add (Mul x (Mul x b)) (Mul x (Mul x c)))))", "x a b c")
 
 run("ex1", "(Add x (Add (Mul a x) (Mul (Mul b x) x)))", "a b x")
