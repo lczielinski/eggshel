2c0ee7b2b8c4a2af364dc7741fc35eaef6e85ec9
Laura Zielinski <lczielinski1@gmail.com>
2025-12-21 12:56:42 -0500

    another optimization

--- runner.py
+++ runner.py
@@ -52,18 +52,6 @@ def generate_ctx(bases):
         result = f"(Tens {bases[i]} {result})"
     return result
 
-# given [x, y, z]
-# generates (rule ((Base 0.0 (Var "x") x_p) (Base 0.0 (Var "y") y_p) (Base 0.0 (Var "z") z_p))
-#   ((Tens (Base 0.0 (Var "x") x_p) (Tens (Base 0.0 (Var "y") y_p) (Base 0.0 (Var "z") z_p))))
-def generate_rule(var_names):
-    conditions = " ".join(f"(Base 0.0 (Var \"{var}\") {var}_p)" for var in var_names)
-    
-    tens_expr = f"(Base 0.0 (Var \"{var_names[-1]}\") {var_names[-1]}_p)"
-    for i in range(len(var_names) - 2, -1, -1):
-        base = f"(Base 0.0 (Var \"{var_names[i]}\") {var_names[i]}_p)"
-        tens_expr = f"(Tens {base} {tens_expr})"
-    return f"(rule ({conditions})\n      ({tens_expr}))"
-
 def run(test_name, expr, ctx):
     expr = generate_expr(expr)
     var_names = ctx.split()
@@ -77,14 +65,15 @@ def run(test_name, expr, ctx):
     with open(file, "w") as f:
         f.write(f"(let ex {expr})\n\n")
         f.write(f"(rule ({" ".join(bases)})\n      ({ctx}))\n\n")
-        f.write(f"(rule ((-> ctx2 ex) (-> ctx1 ctx2))\n      ((-> ctx1 ex)))\n\n")
 
-        f.write("(run-schedule (saturate (run)))\n\n")
-        f.write("(ruleset post)\n\n")
+        f.write(f"(ruleset trans)\n\n")
+        f.write(f"(rule ((-> ctx2 ex) (-> ctx1 ctx2))\n      ((-> ctx1 ex)) :ruleset trans)\n\n")
         
         f.write(f"(relation bounds ({bounds_type}))\n\n")
+        f.write("(ruleset post)\n\n")
         f.write(f"(rule ((-> {ctx} ex))\n      ((bounds {bounds_query})) :ruleset post)\n\n")
-        f.write("(run post 1)\n\n")
+        
+        f.write("(run-schedule (seq (saturate (seq (run) (run trans))) (run post)))\n\n")
         f.write("(print-function bounds 10)\n\n")
 
     src_names = ["Definitions", "Context", "Decombiners", "Operations"]
@@ -99,27 +88,27 @@ def run(test_name, expr, ctx):
         f.write(textwrap.indent(result.stdout, "; "))
         f.write(f"; Took {end - start} seconds")
 
-run("linear/linear2", "(Add x (Mul a x))", "x a")
-run("linear/linear3", "(Add x (Add (Mul a x) (Mul b x)))", "x a b")
-run("linear/linear4", "(Add x (Add (Mul a x) (Add (Mul b x) (Mul c x)))))", "x a b c")
+# run("linear/linear2", "(Add x (Mul a x))", "x a")
+# run("linear/linear3", "(Add x (Add (Mul a x) (Mul b x)))", "x a b")
+# run("linear/linear4", "(Add x (Add (Mul a x) (Add (Mul b x) (Mul c x)))))", "x a b c")
 # run("linear/linear5", "(Add x (Add (Mul a x) (Add (Mul b x) (Add (Mul c x) (Mul d x))))))", "x a b c d")
 # run("linear/linear6", "(Add x (Add (Mul a x) (Add (Mul b x) (Add (Mul c x) (Add (Mul d x) (Mul e x)))))))", "x a b c d e")
 
-run("norm/norm1", "(Sqrt (Mul x x))", "x")
-run("norm/norm2", "(Sqrt (Add (Mul x x) (Mul y y)))", "x y")
-run("norm/norm3", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Mul z z))))", "x y z")
-run("norm/norm4", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Mul a a)))))", "x y z a")
+# run("norm/norm1", "(Sqrt (Mul x x))", "x")
+# run("norm/norm2", "(Sqrt (Add (Mul x x) (Mul y y)))", "x y")
+# run("norm/norm3", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Mul z z))))", "x y z")
+# run("norm/norm4", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Mul a a)))))", "x y z a")
 # run("norm/norm5", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Add (Mul a a) (Mul b b))))))", "x y z a b")
-# run("norm/norm6", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Add (Mul a a) (Add (Mul b b) (Mul c c)))))))", "x y z a b c")
+run("norm/norm6", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Add (Mul a a) (Add (Mul b b) (Mul c c)))))))", "x y z a b c")
 # run("norm/norm7", "(Sqrt (Add (Mul x x) (Add (Mul y y) (Add (Mul z z) (Add (Mul a a) (Add (Mul b b) (Add (Mul c c) (Mul d d))))))))", "x y z a b c d")
 
-run("quad/quad2", "(Add x (Mul x (Mul x a)))", "x a")
-run("quad/quad3", "(Add x (Add (Mul x (Mul x a)) (Mul x (Mul x b))))", "x a b")
+# run("quad/quad2", "(Add x (Mul x (Mul x a)))", "x a")
+# run("quad/quad3", "(Add x (Add (Mul x (Mul x a)) (Mul x (Mul x b))))", "x a b")
 # run("quad/quad4", "(Add x (Add (Mul x (Mul x a)) (Add (Mul x (Mul x b)) (Mul x (Mul x c)))))", "x a b c")
 
-run("ex1", "(Add x (Add (Mul a x) (Mul (Mul b x) x)))", "a b x")
-run("ex2", "(Add a (Sqrt (Mul a b)))", "a b")
-run("ex3", "(Mul (Add a b) (Add b a))", "a b")
-run("ex4", "(Mul (Add a (Mul a b)) (Add c (Mul c d)))", "a b c d")
-run("ex5", "(Add a (Mul a (Sqrt b)))", "a b")
-run("ex6", "(Sqrt (Add (Mul a x) (Sqrt b)))", "a x b")
+# run("ex1", "(Add x (Add (Mul a x) (Mul (Mul b x) x)))", "a b x")
+# run("ex2", "(Add a (Sqrt (Mul a b)))", "a b")
+# run("ex3", "(Mul (Add a b) (Add b a))", "a b")
+# run("ex4", "(Mul (Add a (Mul a b)) (Add c (Mul c d)))", "a b c d")
+# run("ex5", "(Add a (Mul a (Sqrt b)))", "a b")
+# run("ex6", "(Sqrt (Add (Mul a x) (Sqrt b)))", "a x b")
